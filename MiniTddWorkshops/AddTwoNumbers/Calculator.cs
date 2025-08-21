10a5aee6b7cdfa8068d8fd7a05938b8e32288d61
Tobias Eigler <45689354+Teigler@users.noreply.github.com>
2025-08-21 18:06:35 +0200

    initial
    and add first workshop code AddTwoNumbers

---
+++ MiniTddWorkshops/AddTwoNumbers/Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace MiniTddWorkshops.AddTwoNumbers;
+
+/// <summary>
+/// Simple calculator for simple TDD example.
+/// See: https://youtube.com/shorts/04P13NFnvjs
+/// </summary>
+public static class Calculator
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ MiniTddWorkshopsTest/AddTwoNumbers/CalculatorUt.cs
@@ -0,0 +1,19 @@
+﻿using MiniTddWorkshops.AddTwoNumbers;
+
+namespace MiniTddWorkshopsTest.AddTwoNumbers;
+
+[TestFixture]
+public class CalculatorUt
+{
+    [Test]
+    public void Add_TwoNumbers_ReturnsSum()
+    {
+        Assert.That(Calculator.Add(2, 3), Is.EqualTo(5));
+    }
+
+    [Test]
+    public void Add_WithZero_ReturnsOtherNumber()
+    {
+        Assert.That(Calculator.Add(0, 7), Is.EqualTo(7));
+    }
+}
