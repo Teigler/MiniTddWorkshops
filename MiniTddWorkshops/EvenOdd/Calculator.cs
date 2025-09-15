dcad0455e855428b14f0b6a87a81bbe8fe8d7bef
Tobias Eigler <45689354+Teigler@users.noreply.github.com>
2025-09-15 15:58:49 +0200

    Add EvenOdd code for workshop

---
+++ MiniTddWorkshops/EvenOdd/Calculator.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniTddWorkshops.EvenOdd;
+
+public class Calculator
+{
+    public bool? IsEven(int number)
+    {
+        return number % 2 == 0;
+    }
+}
---
+++ MiniTddWorkshopsTest/EvenOdd/CalculatorUt.cs
@@ -0,0 +1,32 @@
+﻿
+using MiniTddWorkshops.EvenOdd;
+
+namespace MiniTddWorkshopsTest.EvenOdd;
+
+[TestFixture]
+public class CalculatorUt
+{
+    [TestCase(2)]
+    [TestCase(4)]
+    [TestCase(6)]
+    public void IsEvenTest(int number)
+    {
+        var calculator = CreateCalculator();
+        Assert.IsTrue(calculator.IsEven(number));
+    }
+
+    [TestCase(1)]
+    [TestCase(3)]
+    [TestCase(5)]
+    public void IsOddTest(int number)
+    {
+        var calculator = CreateCalculator();
+        Assert.IsFalse(calculator.IsEven(number));
+    }
+
+    private static Calculator CreateCalculator()
+    {
+        var calculator = new Calculator();
+        return calculator;
+    }
+}
